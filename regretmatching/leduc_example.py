505f34407103507c9363050109e3f42d7eb62aaa
Chris Nicholls <cgnicholls@googlemail.com>
2017-12-28 22:30:11 +0000

    Updated Leduc CFR so you can play against it

--- regretmatching/leduc_example.py
+++ regretmatching/leduc_example.py
@@ -9,6 +9,36 @@ import pickle
 from time import time
 from argparse import ArgumentParser
 
+
+def play_against(game, user_player, strategy):
+	""" Given a strategy in the form of a dictionary from information sets to probability
+	distributions over actions, the user can play against it. The user plays as 'user_player'
+	(either 1 or 2).
+	"""
+	# First reset the game
+	player, information_set, terminal, payoffs = game.reset()
+	while not terminal:
+		if player == user_player:
+			print("Player {} to play".format(player))
+			print("Information set: {}".format(information_set))
+			print("Please enter action (0 = fold, 1 = call, 2 = raise):")
+			action = int(input())
+			#action = 1
+		else:
+			# Sometimes the information set doesn't exist in the strategy (since we're chance sampling)
+			if not information_set in strategy:
+				action = None
+			else:
+				prob_dist = []
+				for a in [0,1,2]:
+					if a in strategy[information_set]:
+						prob_dist.append(strategy[information_set][a])
+					else:
+						prob_dist.append(0.0)
+				action = np.random.choice([0,1,2], p=prob_dist)
+		player, information_set, terminal, payoffs = game.play_action(action)
+	return payoffs
+
 if __name__ == "__main__":
 
 	parser = ArgumentParser()
@@ -17,6 +47,7 @@ if __name__ == "__main__":
 	parser.add_argument("--num_iters", help="The number of iterations to run CFR for")
 
 	args = parser.parse_args()
+	# If we don't load a file, then we compute a maximin strategy using CFR.
 	if not args.load_file:
 		game = LeducCFR()
 		num_iters = 10000
@@ -28,6 +59,17 @@ if __name__ == "__main__":
 		with open('average_strategy-' + str(int(time())) + '.strategy', 'wb') as f:
 			pickle.dump(average_strategy, f)
 	else:
+		# Otherwise we load the specified strategy and play against it
 		with open(args.load_file, 'rb') as f:
 			average_strategy = pickle.load(f)
 			print(average_strategy)
+
+			# Now the user can play against the strategy
+			game = LeducCFR()
+			all_payoffs = {1: [], 2: []}
+			while True:
+				payoffs = play_against(game, 2, average_strategy)
+				all_payoffs[1].append(payoffs[1])
+				all_payoffs[2].append(payoffs[2])
+				print("Payoffs: {}".format(payoffs))
+				print("Average payoffs: {}".format({i: np.mean(all_payoffs[i]) for i in all_payoffs}))
